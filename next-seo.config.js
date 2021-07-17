7efd2d040a1609ef61e47702adf7f3a7d0d2f2eb
TheBoringDude <iamcoderx@gmail.com>
2021-07-17 20:46:20 +0800

    feat: seo configuration

---
+++ next-seo.config.js
@@ -0,0 +1,115 @@
+module.exports = {
+  titleTemplate: "%s | World of CryptoPups",
+  openGraph: {
+    type: "website",
+    locale: "en_PH",
+    url: "https://www.cryptopups.cf/",
+    site_name: "World of CryptoPups",
+    images: [
+      {
+        url: "https://cryptopups.cf/images/cosmic.png",
+        height: 400,
+        width: 200,
+      },
+      {
+        url: "https://cryptopups.cf/logo.png",
+        height: 200,
+        width: 400,
+        alt: "The World of CryptoPups",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@apppllleee_pie",
+    cardType: "summary_large_image",
+  },
+  additionalMetaTags: [
+    {
+      name: "msapplication-TileColor",
+      content: "#ffffff",
+    },
+    {
+      name: "msapplication-TileImage",
+      content: "/ms-icon-144x144.png",
+    },
+    {
+      name: "theme-color",
+      content: "#ffffff",
+    },
+  ],
+  additionalLinkTags: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "57x57",
+      href: "/apple-icon-57x57.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "60x60",
+      href: "/apple-icon-60x60.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "72x72",
+      href: "/apple-icon-72x72.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "76x76",
+      href: "/apple-icon-76x76.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "114x114",
+      href: "/apple-icon-114x114.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "120x120",
+      href: "/apple-icon-120x120.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "144x144",
+      href: "/apple-icon-144x144.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      href: "/apple-icon-152x152.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-icon-180x180.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      href: "/android-icon-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      href: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    {
+      rel: "manifest",
+      href: "/manifest.json",
+    },
+  ],
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { DefaultSeo } from "next-seo";
+import type { AppProps } from "next/app";
+import SEO from "../next-seo.config";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;

dc2cf8f8aff1dc5e6475a7dcde817ba5b8fde1e3
TheBoringDude <iamcoderx@gmail.com>
2021-07-18 22:40:58 +0800

    fix (dps-calculator): wrong api url

--- modules/dps-calc/fetch.tsx
+++ modules/dps-calc/fetch.tsx
@@ -9,8 +9,8 @@ type DPS_FetchProps = {
 };
 
 const PUPSKINS_API = `https://wax.api.atomicassets.io/atomicassets/v1/assets?owner={{owner}}&collection_name=cryptopuppie&schema_name=pupskincards&page=1&limit=1000&order=desc&sort=asset_id`;
-const PUPPYCARDS_API = `https://wax.api.atomicassets.io/atomicassets/v1/assets?owner={{owner}}&collection_name=puppycards&schema_name=pupskincards&page=1&limit=1000&order=desc&sort=asset_id`;
-const PUPITEMS_API = `https://wax.api.atomicassets.io/atomicassets/v1/assets?owner={{owner}}&collection_name=pupitems&schema_name=pupskincards&page=1&limit=1000&order=desc&sort=asset_id`;
+const PUPPYCARDS_API = `https://wax.api.atomicassets.io/atomicassets/v1/assets?owner={{owner}}&collection_name=cryptopuppie&schema_name=puppycards&page=1&limit=1000&order=desc&sort=asset_id`;
+const PUPITEMS_API = `https://wax.api.atomicassets.io/atomicassets/v1/assets?owner={{owner}}&collection_name=cryptopuppie&schema_name=pupitems&page=1&limit=1000&order=desc&sort=asset_id`;
 
 const DPS_Fetch = ({ wallet }: DPS_FetchProps) => {
   // pupskins

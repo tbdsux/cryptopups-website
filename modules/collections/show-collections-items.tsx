1b804828d28f7dbdd692f601e0e9059090988901
TheBoringDude <iamcoderx@gmail.com>
2021-07-30 14:39:53 +0800

    (my-collections): update design for list-items

--- modules/collections/show-collections-items.tsx
+++ modules/collections/show-collections-items.tsx
@@ -7,12 +7,12 @@ type ShowCollectionsItemsProps = {
 
 const ShowCollectionsItems = ({ data }: ShowCollectionsItemsProps) => {
   return (
-    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 h-112 py-4 rounded-xl overflow-auto border">
+    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-10 p-6 h-112 py-4 rounded-xl overflow-auto border">
       {data.map((k, index) => (
-        <li key={index} className="inline-flex flex-col items-center">
+        <li key={index} className="inline-flex flex-col items-center shadow-2xl rounded-xl py-2">
           <Image
             src={`https://wax.atomichub.io/ipfs/${k.data.img}`}
-            height="300"
+            height="400"
             width="300"
             objectFit="contain"
             alt={k.name.trim()}

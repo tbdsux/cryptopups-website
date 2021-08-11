2d3ee0b2f1636d79da26e38a11d0b41cdd067f68
TheBoringDude <iamcoderx@gmail.com>
2021-08-11 20:38:15 +0800

    (collections): show no items text if no data in collections

--- modules/collections/show-collections-items.tsx
+++ modules/collections/show-collections-items.tsx
@@ -7,22 +7,30 @@ type ShowCollectionsItemsProps = {
 
 const ShowCollectionsItems = ({ data }: ShowCollectionsItemsProps) => {
   return (
-    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-10 p-6 h-112 py-4 rounded-xl overflow-auto border">
-      {data.map((k, index) => (
-        <li
-          key={index}
-          className="flex flex-col items-center justify-center shadow-2xl rounded-xl pt-1 pb-3"
-        >
-          <Image
-            src={`https://gateway.pinata.cloud/ipfs/${k.data.img}`}
-            height="400"
-            width="300"
-            objectFit="contain"
-            alt={k.name.trim()}
-          />
-          <span>{k.name.trim()}</span>
-        </li>
-      ))}
+    <ul
+      className={`${
+        data.length > 0 && 'grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-10'
+      } p-6 h-112 py-4 rounded-xl overflow-auto border`}
+    >
+      {data.length > 0 ? (
+        data.map((k, index) => (
+          <li
+            key={index}
+            className="flex flex-col items-center justify-center shadow-2xl rounded-xl pt-1 pb-3"
+          >
+            <Image
+              src={`https://gateway.pinata.cloud/ipfs/${k.data.img}`}
+              height="400"
+              width="300"
+              objectFit="contain"
+              alt={k.name.trim()}
+            />
+            <span>{k.name.trim()}</span>
+          </li>
+        ))
+      ) : (
+        <p>You have no items...</p>
+      )}
     </ul>
   );
 };

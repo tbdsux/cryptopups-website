e1a5d43a08a7ce1ec3588b89c6cbb6931cbf66c7
TheBoringDude <iamcoderx@gmail.com>
2021-08-11 22:46:26 +0800

    fix: move domain to worldofcryptopups.cf

--- modules/collections/authenticate.tsx
+++ modules/collections/authenticate.tsx
@@ -15,7 +15,7 @@ import { APIResponseProps } from '../../types/responses';
 
 const chainId = '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4';
 const endpoint = 'https://wax.greymass.com';
-const dapp = 'cryptopups.cf';
+const dapp = 'worldofcryptopups.cf';
 
 // wax cloud
 const wax = new waxjs.WaxJS(endpoint, undefined, undefined, false);

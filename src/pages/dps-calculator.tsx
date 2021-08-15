6f83026d677dab440e5f2250f6c7b987ea1cb183
TheBoringDude <iamcoderx@gmail.com>
2021-08-16 07:04:38 +0800

    (dps-calc): implement and allow executions from query

--- src/pages/dps-calculator.tsx
+++ src/pages/dps-calculator.tsx
@@ -1,3 +1,32 @@
-import DPSCalculatorPage from "../modules/dps-calc";
+import { GetServerSideProps } from 'next';
+import DPSCalculatorPage from '../modules/dps-calc';
+import { PUPITEMS_API, PUPPYCARDS_API, PUPSKINS_API } from '../modules/dps-calc/apilinks';
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { waxid } = query;
+
+  if (waxid) {
+    const wallet = Array.isArray(waxid) ? waxid.join() : waxid;
+
+    const pupskins = await (await fetch(PUPSKINS_API.replace('{{owner}}', wallet))).json();
+    const pupcards = await (await fetch(PUPPYCARDS_API.replace('{{owner}}', wallet))).json();
+    const pupitems = await (await fetch(PUPITEMS_API.replace('{{owner}}', wallet))).json();
+
+    return {
+      props: {
+        wallet,
+        data: {
+          pupskins,
+          pupcards,
+          pupitems
+        }
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+};
 
 export default DPSCalculatorPage;

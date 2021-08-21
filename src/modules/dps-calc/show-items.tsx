3dedb19bef179518bfddac5a6bb262a191585eff
TheBoringDude <iamcoderx@gmail.com>
2021-08-21 23:42:37 +0800

    fix wrong event timer

--- src/modules/dps-calc/show-items.tsx
+++ src/modules/dps-calc/show-items.tsx
@@ -1,14 +1,11 @@
-import { DPSComponentProps } from "./apitypes";
+import { DPSComponentProps } from './apitypes';
 
 const ShowItems = ({ owner, data }: DPSComponentProps) => {
   return (
     <ul className="h-112 overflow-auto border p-2 rounded-lg">
       {data.map((d, index) =>
         d.owner === owner ? (
-          <li
-            key={index}
-            className="flex items-center justify-between border-b p-2"
-          >
+          <li key={index} className="flex items-center justify-between border-b p-2">
             <p>{d.data.name.trim()}</p>
             <strong className="text-gray-800">{d.data.DPS}</strong>
           </li>

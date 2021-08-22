269749f782db84f2508daf68e17d87e2bffc4ec8
TheBoringDude <iamcoderx@gmail.com>
2021-08-23 07:04:51 +0800

    feat (dps-calc): filter out items / dps results

--- src/modules/dps-calc/show-items.tsx
+++ src/modules/dps-calc/show-items.tsx
@@ -1,13 +1,40 @@
-import { DPSComponentProps } from './apitypes';
+import { Datum, DPSComponentProps } from './apitypes';
+
+type FilteredDatum = {
+  [x: string]: {
+    data: Datum;
+    count: number;
+  };
+};
+
+const datumFilter = (data: Datum[]) => {
+  var x: FilteredDatum = {};
+
+  data.forEach((i) => {
+    x[i.name] = {
+      data: i,
+      count: (x[i.name] ? x[i.name].count : 0) + 1
+    };
+  });
+
+  return x;
+};
 
 const ShowItems = ({ owner, data }: DPSComponentProps) => {
   return (
     <ul className="h-112 overflow-auto border p-2 rounded-lg">
-      {data.map((d, index) =>
+      {Object.entries(datumFilter(data)).map(([, { data: d, count }], index) =>
         d.owner === owner ? (
           <li key={index} className="flex items-center justify-between border-b p-2">
-            <p>{d.data.name.trim()}</p>
-            <strong className="text-gray-800">{d.data.DPS}</strong>
+            <p>
+              {d.data.name.trim()}{' '}
+              {count > 1 && (
+                <span title={`${count} Items`} className="underline text-gray-700 text-sm">
+                  ({count})
+                </span>
+              )}
+            </p>
+            <strong className="text-gray-800">{Number(d.data.DPS) * Number(count)}</strong>
           </li>
         ) : null
       )}

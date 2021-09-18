0e5d44b8f6ad60d9365a5f02434e8ad8e99c322b
TheBoringDude <iamcoderx@gmail.com>
2021-09-18 17:44:34 +0800

    add show of elementals project

--- src/modules/index/index.tsx
+++ src/modules/index/index.tsx
@@ -1,4 +1,6 @@
 import { NextSeo } from 'next-seo';
+import Image from 'next/image';
+import ElementalsBg from '../../../public/elementals.jpg';
 import DefaultLayout from '../../layouts/default';
 import DiscoverContainer from './discover';
 import CurrentEventModal from './events/current-event-modal';
@@ -38,6 +40,28 @@ const IndexPage = () => {
         <hr className="border-gray-200 w-5/6 mx-auto" />
 
         <SmashKartsTournamentEvent />
+      </div>
+
+      <div className="relative">
+        <div className="py-20 w-5/6 mx-auto">
+          <a
+            title="See Elementals Project"
+            href="https://elementals.worldofcryptopups.cf/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white relative z-30"
+          >
+            <div className="flex items-center justify-between">
+              <h3 className="font-black text-5xl">Elemental Pups</h3>
+            </div>
+          </a>
+          <Image
+            src={ElementalsBg}
+            alt="World of Cryptopups"
+            layout="fill"
+            className="w-full h-full inset-0 z-20 object-cover brightness-20"
+          />
+        </div>
       </div>
 
       {/*

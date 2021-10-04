e6c5d3fcd520eea64fd42e6bddec16b98bdb0396
TheBoringDude <iamcoderx@gmail.com>
2021-10-04 22:23:44 +0800

    (gallery): update gallery to use templates from atomichub api

--- src/modules/gallery/gallery-container.tsx
+++ src/modules/gallery/gallery-container.tsx
@@ -1,14 +1,17 @@
 import GalleryImages from './gallery-images';
 import GalleryProvider from './gallery-provider';
 import GallerySidebar from './gallery-sidebar';
+import { useImages } from './useImages';
 
 // main container
 const GalleryContainer = () => {
+  const images = useImages();
+
   return (
     <GalleryProvider>
       <div className="flex flex-col md:flex-row items-start justify-between">
-        <GallerySidebar />
-        <GalleryImages />
+        <GallerySidebar images={images} />
+        <GalleryImages images={images} />
       </div>
     </GalleryProvider>
   );
---
+++ src/modules/gallery/useImages.ts
@@ -0,0 +1,20 @@
+import useSWR from 'swr';
+import { ImagesResponseProps } from '../../types/template';
+
+const useImages = () => {
+  const { data } = useSWR<ImagesResponseProps>(
+    'https://wax.api.atomicassets.io/atomicassets/v1/templates?collection_name=cryptopuppie&schema_name=pupskincards%2Cpupitems%2Cpuppycards&page=1&limit=1000&order=desc&sort=created'
+  );
+
+  if (!data) {
+    return null;
+  }
+
+  if (!data.success) {
+    return false;
+  }
+
+  return data.data;
+};
+
+export { useImages };

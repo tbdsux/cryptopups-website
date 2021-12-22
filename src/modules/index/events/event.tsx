d9632cdbbbe603bdff575440f648c85cec9e8fdc
TheBoringDude <iamcoderx@gmail.com>
2021-12-22 22:07:34 +0800

    Change way of adding / setting new events. Also, remove existing past events.

--- src/modules/index/events/event.tsx
+++ src/modules/index/events/event.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
-import eventImg from '../../../../../public/_events/smashkarts_event.png';
-import Container from '../../../../components/container';
-import EventsTimer from '../timer';
+import eventImg from '../../../../public/_events/infinitevoidsreturn.png';
+import Container from '../../../components/container';
 
-const SmashKartsTournamentEvent = () => {
+const CurrentEventContainer = () => {
   return (
     <div className="pt-24">
       <Container className="w-4/5 mb-12">
@@ -12,23 +11,24 @@ const SmashKartsTournamentEvent = () => {
             <Image
               src={eventImg}
               alt="Season One Pass"
-              width="1400"
-              height="800"
+              width="850"
+              height="850"
               placeholder="blur"
               className="object-contain"
             />
           </div>
           <div>
             <h3 className="text-4xl lg:text-5xl xl:text-6xl uppercase font-black tracking-wide text-warmGray-900">
-              Smash Karts Tournament
+              Infinite Void Returns
             </h3>
             <p className="text-base lg:text-lg xl:text-xl tracking-wide mt-3 xl:mt-4 text-gray-700">
-              Join the tournament for <strong>amazing prices</strong> and fun! Smash Karts is a 3D
-              multiplayer kart battle game. Drive your go-kart, pick up weapons, and blow up other
-              karts to win!
+              A familiar rift opens in the World of Cryptopups... An upcoming threat is imminent...
+              <br />
+              <br />
+              <q>Gaze into nothingness</q>
             </p>
 
-            <a
+            {/* <a
               href="https://events.worldofcryptopups.cf/smashkarts-tournament"
               target="_blank"
               rel="noreferrer"
@@ -49,18 +49,18 @@ const SmashKartsTournamentEvent = () => {
                 />
               </svg>{' '}
               <span className="ml-2 font-bold">View Event</span>
-            </a>
+            </a> */}
           </div>
         </div>
 
-        <div className="float-right flex flex-col">
+        {/* <div className="float-right flex flex-col">
           <div className="inline-flex items-end my-1">
             <EventsTimer date="19 Sep 2021 04:00 PM UTC" duration={48} />
           </div>
-        </div>
+        </div> */}
       </Container>
     </div>
   );
 };
 
-export default SmashKartsTournamentEvent;
+export default CurrentEventContainer;

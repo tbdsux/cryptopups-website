0203b3510ab106290f0c00d81833b217217ed50e
TheBoringDude <iamcoderx@gmail.com>
2022-08-24 19:29:21 +0800

    feat: implement `nextjs-progressbar` for a loader animation while navigating through pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,25 @@ import { UseAtomicAssetsProvider } from '@cryptopuppie/useatomicassets';
 import { UseEOSProvider } from '@cryptopuppie/useeoschain';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import SEO from '../../next-seo.config';
 import { atomicassets, chainId, dApp, endpoint } from '../lib/config';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WaxAuthProvider net={{ chainId, endpoint, dApp }}>
-      <UseEOSProvider endpoint={endpoint}>
-        <UseAtomicAssetsProvider endpoint={atomicassets}>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-        </UseAtomicAssetsProvider>
-      </UseEOSProvider>
-    </WaxAuthProvider>
+    <>
+      <NextNProgress color="#fff" />
+
+      <WaxAuthProvider net={{ chainId, endpoint, dApp }}>
+        <UseEOSProvider endpoint={endpoint}>
+          <UseAtomicAssetsProvider endpoint={atomicassets}>
+            <DefaultSeo {...SEO} />
+            <Component {...pageProps} />
+          </UseAtomicAssetsProvider>
+        </UseEOSProvider>
+      </WaxAuthProvider>
+    </>
   );
 }
 export default MyApp;

3de79017376c46da7071434c5f34a8a12ff8569e
TheBoringDude <iamcoderx@gmail.com>
2022-09-12 16:40:46 +0800

    fix: lightbox of image in gallery cannot be closed in mobile devices

--- src/modules/gallery/gallery-lightbox.tsx
+++ src/modules/gallery/gallery-lightbox.tsx
@@ -18,13 +18,12 @@ const ImageLightbox = ({ src, alt, className }: ImageLightboxProps) => {
           onClose={() => setOpen(undefined)}
           images={[open]}
           className="fixed w-full h-full bg-black/80 z-50 inset-0 flex items-center justify-center"
-          containerClassname="z-50 flex items-center justify-between"
           overlayClassname="absolute h-full w-full z-30"
           arrowsClassName={{
             icons: '',
             buttons: 'h-6 w-6 text-white'
           }}
-          imageClassName="mx-4 h-screen"
+          imageClassName="h-screen w-4/5 mx-auto object-contain"
           hideArrowButtons
         />
       )}

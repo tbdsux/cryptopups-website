ce06df8a67fffe91106c3d9b7d0fa047e0ff4538
TheBoringDude <iamcoderx@gmail.com>
2022-10-07 16:07:15 +0800

    profile: add a copy button near token to for an easy-copy

--- src/modules/profile/pup-profile.tsx
+++ src/modules/profile/pup-profile.tsx
@@ -1,5 +1,7 @@
 import { useWaxUser } from '@cryptopuppie/next-waxauth';
+import { CheckIcon, DuplicateIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import useGetToken from '../../hooks/useGetToken';
 import useGetWaxProfile from '../../hooks/useGetWaxProfile';
 
@@ -8,13 +10,44 @@ const PupProfile = () => {
   const authData = useGetToken();
   const profile = useGetWaxProfile();
 
+  const [copied, setCopied] = useState(false);
+
+  const copyToken = () => {
+    if (!authData) return;
+
+    navigator.clipboard.writeText(authData.token);
+    setCopied(true);
+  };
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+    }
+  }, [copied]);
+
   return (
     <div className="bg-pastel-violet text-white p-12 rounded-3xl">
       <div className="flex items-center justify-between flex-wrap">
         <span className="font-alt uppercase tracking-wide text-xl">Pup Profile</span>
 
-        <p className="font-medium w-full sm:w-auto text-right text-lg">
-          Token ID: <span>{authData?.token}</span>
+        <p className="font-medium w-full sm:w-auto text-right sm:text-lg flex items-center justify-end">
+          Token ID:
+          <span className="inline-flex items-center ml-2">
+            {authData?.token}
+            <button
+              onClick={copyToken}
+              title={copied ? 'Token has been copied' : 'Copy Token'}
+              className=" ml-1"
+            >
+              {copied ? (
+                <CheckIcon aria-hidden="true" className="h-5 w-5" />
+              ) : (
+                <DuplicateIcon aria-hidden="true" className="h-5 w-5" />
+              )}
+            </button>
+          </span>
         </p>
       </div>
 

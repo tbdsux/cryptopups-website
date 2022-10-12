6c78f97150fa7b0f7af3b8d3f9f831cb48319bc3
TheBoringDude <iamcoderx@gmail.com>
2022-10-12 14:09:59 +0800

    profile: update rank changes

--- src/modules/profile/season-dps/rank.ts
+++ src/modules/profile/season-dps/rank.ts
@@ -1,17 +1,13 @@
 const rankIdentifier = (dps: number) => {
-  if (dps >= 3000 && dps < 5000) return 'Warrior Pups';
+  if (dps >= 10000 && dps < 20000) return 'Overlord Pups';
 
-  if (dps >= 5000 && dps < 8000) return 'Knight Pups';
+  if (dps >= 20000 && dps < 35000) return 'Apocalyptic Pups';
 
-  if (dps >= 8000 && dps < 10000) return 'Overlord Pups';
+  if (dps >= 35000 && dps < 60000) return 'Above All Pups';
 
-  if (dps >= 10000 && dps < 25000) return 'Pups Apocalypse';
+  if (dps >= 60000 && dps < 100000) return 'Infinity Pups';
 
-  if (dps >= 25000 && dps < 70000) return 'Pups Above All';
-
-  if (dps >= 70000 && dps < 145000) return 'Doggos of Infinity';
-
-  if (dps >= 145000) return 'Doggos of Eternity';
+  if (dps >= 100000) return 'Eternity Pups';
 
   return '-';
 };

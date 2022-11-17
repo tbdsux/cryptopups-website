2d8142748dd5e1b4437b086f953fd649eb3ce224
TheBoringDude <iamcoderx@gmail.com>
2022-11-18 00:00:38 +0800

    fix: set z-index values accordingly so that react-toastify popups show in the front

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
   return (
     <header
       ref={headerRef}
-      className="py-6 bg-black/50 hover:bg-black duration-500 fixed w-full z-[9999]"
+      className="py-6 bg-black/50 hover:bg-black duration-500 fixed w-full z-[99]"
     >
       <Container className="w-11/12 2xl:w-5/6 text-center">
         <MobileMenu />
